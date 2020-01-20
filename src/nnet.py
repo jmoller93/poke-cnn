Small fixes and added in softmax layer in neural net

src/nnet.py
@@ -19,14 +19,16 @@ class Net(nn.Module):
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 4)
         self.fc1 = nn.Linear(16*5*5, 32)
-        self.fc2 = nn.Linear(32, 8)
+        self.fc2= nn.Linear(32, 8)
+        self.fc3 = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
 
     def num_flat_features(self, x):