27b488a21c0e1c015a7614d88ae77510efefe7f8
Joshua Moller <jmoller@uchicago.edu>
2020-01-22 18:35:29 -0600

    Now up to 96% accuracy on dataset with 10 epochs

--- src/nnet.py
+++ src/nnet.py
@@ -16,15 +16,15 @@ class Net(nn.Module):
     def __init__(self,f,p):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 40, 5)
-        self.conv2 = nn.Conv2d(40, 60, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(4*4*60, 8)
+        self.fc1 = nn.Linear(12*12*40, 512)
+        self.fc2 = nn.Linear(512, 8)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
         x = x.view(-1, self.num_flat_features(x))
-        x = self.fc1(x)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
         return x
 
     def num_flat_features(self, x):
